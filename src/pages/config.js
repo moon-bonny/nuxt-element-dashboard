642e9cd5f947ca48d049f668a008f83af366e7d2
moon_o-o <lanyj123@163.com>
2019-04-22 15:00:19 +0800

    demo提交

---
+++ src/pages/config.js
@@ -0,0 +1,135 @@
+import format from '@/const/filter'
+export default {
+  searchForm: [
+    {
+      $el: {placeholder: '请输入'},
+      label: '组件名称',
+      $id: 'component_name',
+      $type: 'input'
+    },
+    {
+      $el: {placeholder: '请选择分类'},
+      label: '分类',
+      $id: 'type',
+      $type: 'select',
+      $options: [
+        '前端组件',
+        '测试子组件',
+        '分布式工具',
+        '应用服务',
+        '数据存储'
+      ].map(f => ({label: f, value: f}))
+    },
+    {
+      $el: {placeholder: '请选择状态'},
+      label: '状态',
+      $id: 'status',
+      $type: 'select',
+      $options: ['上架', '下架'].map(f => ({label: f, value: f}))
+    }
+  ],
+  form: [
+    {
+      $type: 'input',
+      $id: 'component_name',
+      label: '组件名称',
+      rules: [{required: true, message: '请输入组件名称', trigger: 'blur'}]
+    },
+    {
+      $type: 'select',
+      $id: 'type',
+      label: '分类',
+      rules: [{required: true, message: '请选择分类', trigger: 'blur'}],
+      $type: 'select',
+      $options: [
+        '前端组件',
+        '测试子组件',
+        '分布式工具',
+        '应用服务',
+        '数据存储'
+      ].map(f => ({label: f, value: f}))
+    },
+    {
+      $type: 'input',
+      $id: 'version',
+      label: '版本',
+      rules: [{required: true, message: '请输入版本', trigger: 'blur'}]
+    },
+    {
+      $type: 'input',
+      $id: 'dev_lang',
+      label: '开发语言',
+      rules: [{required: true, message: '请输入开发语言', trigger: 'blur'}]
+    },
+    {
+      $type: 'date-picker',
+      $id: 'last_update_time',
+      label: '最后更新时间',
+      rules: [{required: true, message: '请选择最后更新时间', trigger: 'blur'}],
+      $el: {
+        // type: 'daterange',   // 如果 type 为 daterange，则获取到的是一个数组
+        placeholder: '请选择',
+        valueFormat: 'timestamp' // 不使用 value-format 属性，则获取到的是一个空对象，格式请参考 element-ui 组件 date-picker 的日期格式部分
+      }
+    },
+    {
+      $type: 'select',
+      $id: 'status',
+      label: '状态',
+      rules: [{required: true, message: '请选择状态', trigger: 'blur'}],
+      $options: ['上架', '下架'].map(f => ({label: f, value: f})),
+      $el: {
+        placeholder: '请选择状态'
+      }
+    }
+  ],
+  formAttrs: {labelWidth: '120px'},
+  url: '/api/get-index-list',
+  extraButtons: [
+    {
+      type: 'primary',
+      text: '查看',
+      atClick: row => {}
+    },
+    {
+      text: '编辑',
+      atClick: row => {}
+    },
+    {
+      show: function(row) {
+        this.text = row.status === 1 ? '下架' : '上架'
+        return this.text
+      },
+      atClick: function(row) {
+        let text = row.status === 1 ? '下架' : '上架'
+        return Promise.resolve(alert(text + ':' + row.component_name))
+      }
+    }
+  ],
+  // dataPath: 'items',
+  // totalPath: 'total_count',
+  columns: [
+    {type: 'selection'},
+    {prop: 'component_name', label: '组件名称'},
+    {prop: 'type', label: '分类'},
+    {prop: 'version', label: '版本'},
+    {prop: 'dev_lang', label: '开发语言'},
+    {
+      prop: 'last_update_time',
+      label: '最后更新时间',
+      minWidth: '90px',
+      formatter: row => format(row.last_update_time, 'YYYY-MM-DD')
+    },
+    {
+      prop: 'status',
+      label: '状态',
+      formatter: row => (row.status === 1 ? '上架' : '下架')
+    }
+  ],
+  operationAttrs: {
+    minWidth: '110px'
+  },
+  canDelete() {
+    return false
+  }
+}
---
+++ src/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="index">
+    <!-- <img class="home-img" :src="$store.state.meta.homePageImg" alt="homeImg" /> -->
+    <el-data-table v-bind="$data" :hasEdit="false"> </el-data-table>
+  </div>
+</template>
+
+<script>
+import ElDataTable from '../components/el-data-table/el-data-table.vue'
+import config from './config'
+export default {
+  components: {ElDataTable},
+  data() {
+    return config
+  },
+  methods: {}
+}
+</script>
+<style lang="less">
+.index {
+  .home-img {
+    width: 100%;
+  }
+}
+</style>
